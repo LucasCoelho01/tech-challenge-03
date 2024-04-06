b09abf7e8b551a374ff082b1ea06c8867fbe4bd9
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2024-04-05 21:51:59 -0300

    First commit

---
+++ src/main/java/com/lucas/techchallenge/TechChallenge03Application.java
@@ -0,0 +1,13 @@
+package com.lucas.techchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallenge03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallenge03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/techchallenge/TechChallenge03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.techchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallenge03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
